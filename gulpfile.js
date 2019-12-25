6e449f4596a1da6e641bdbb5b95c4c8af7d90952
alexvlife <alexvlife@gmail.com>
2019-12-25 12:35:46 +0300

    module8-task1 - оптимизирует изображения
    подключены пакеты:
    - gulp-imagemin;
    - gulp-webp.

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -23,6 +25,22 @@ gulp.task("css", function () {
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo(),
+    ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 80}))
+    .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("server", function () {
